248d352d7766e884c460bb36858b9506336ae129
DredKolln <odracirkdred@gmail.com>
2023-08-03 11:34:59 -0300

    main create project

---
+++ demo/demo/src/main/java/com/kolln/demo/model/Director.java
@@ -0,0 +1,12 @@
+package com.kolln.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "directors")
+public class Director {
+    @Id
+    private
+}
---
+++ demo/demo/src/test/java/com/kolln/demo/RentamovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolln.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentamovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
